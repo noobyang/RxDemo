cf60847c3f6ad061e377f644256ce6a614e8dabe
liyang <liyang@oaserver.dw.gdbbk.com>
2018-04-19 09:52:45 +0800

    init project

---
+++ app/src/main/java/com/lee/rxdemo/RxApplication.java
@@ -0,0 +1,17 @@
+package com.lee.rxdemo;
+
+import android.app.Application;
+
+/**
+ * RxApplication
+ * </p>
+ * Created by LiYang on 2018/4/19.
+ */
+
+public class RxApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
